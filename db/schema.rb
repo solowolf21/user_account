ab828c698b081c50d112e13a81e7e4221b2f2660
Sen Zhang <sen.zhang@appfolio.com>
2014-02-25 16:44:24 -0800

    sz - upgrade to rails 4.0 and remove strong param gem

--- db/schema.rb
+++ db/schema.rb
@@ -9,16 +9,16 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131126032144) do
+ActiveRecord::Schema.define(version: 20131126032144) do
 
-  create_table "movies", :force => true do |t|
+  create_table "movies", force: true do |t|
     t.string   "title"
     t.string   "rating"
-    t.decimal  "total_gross",     :precision => 10, :scale => 0
-    t.datetime "created_at",                                     :null => false
-    t.datetime "updated_at",                                     :null => false
+    t.decimal  "total_gross",     precision: 10, scale: 0
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
     t.text     "description"
     t.date     "released_on"
     t.string   "cast"
@@ -27,15 +27,15 @@ ActiveRecord::Schema.define(:version => 20131126032144) do
     t.string   "image_file_name"
   end
 
-  create_table "reviews", :force => true do |t|
+  create_table "reviews", force: true do |t|
     t.string   "name"
     t.integer  "stars"
     t.text     "comment"
     t.integer  "movie_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "reviews", ["movie_id"], :name => "index_reviews_on_movie_id"
+  add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id", using: :btree
 
 end
