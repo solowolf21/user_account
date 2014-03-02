2fece94add14ef7d024c502a2ef18ef2cfe1d0c6
Sen Zhang <sen.zhang@appfolio.com>
2014-03-01 22:12:13 -0800

    sz - add user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+private
+  def require_signin
+    unless find_current_user
+      session[:intended_url] = request.url
+      flash[:alert] = 'Please Sign In First!'
+      redirect_to new_session_path
+    end
+  end
+
+  def find_current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_user?(user)
+    find_current_user == user
+  end
+
+  helper_method :find_current_user, :current_user?
 end
