7366837966324ad080f8135a49dd248c0f091a08
Sen Zhang <sen.zhang@appfolio.com>
2014-03-15 12:09:12 -0700

    sz - add tests for genre and movie controller

--- test/models/genre_test.rb
+++ test/models/genre_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class GenreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_name
+    g = Genre.new
+    assert !g.valid?
+    assert_equal ["Name can't be blank"], g.errors.full_messages
+
+    g.name = 'Action'
+    assert g.valid?
+
+    Genre.create_exemplar!(:name => 'Action')
+
+    assert !g.valid?
+    assert_equal ["Name has already been taken"], g.errors.full_messages
+
+    g.name = "Comedy"
+    assert g.valid?
+  end
+
+  def test_getter_and_setter
+    g = Genre.create_exemplar!(:name => 'Action')
+    assert_equal 'Action', g.name
+
+    g.name = 'Comedy'
+    g.save!
+    assert_equal 'Comedy', g.name
+  end
+
 end
