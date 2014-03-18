2f3e24a2585fb8e6c9cd9b9b3cb66db160c23dd7
Sen Zhang <sen.zhang@appfolio.com>
2014-03-17 20:16:09 -0700

    sz - move genres, likers and liked movies to sider, and enable dynamic page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def page_title
+    if content_for?(:title)
+      "User Account - #{content_for(:title)}"
+    else
+      'User Account'
+    end
+  end
 end
--- test/view_helpers/application_helper_test.rb
+++ test/view_helpers/application_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
+  def test_page_title
+    assert_false content_for?(:title)
+
+    assert_equal 'User Account', page_title
+
+    content_for(:title, 'Foobar')
+    assert content_for?(:title)
+    assert_equal 'User Account - Foobar', page_title
+  end
 end
